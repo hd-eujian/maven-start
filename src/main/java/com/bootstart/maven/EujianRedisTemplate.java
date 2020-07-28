09b6530fa486d30ff09793e1271f8e9c3fb19876
yeyongjian <yeyongjian@gac-nio.com>
2020-07-28 12:01:27 +0800

    maven

--- src/main/java/com/bootstart/maven/EujianRedisTemplate.java
+++ src/main/java/com/bootstart/maven/EujianRedisTemplate.java
@@ -1,5 +1,6 @@
 package com.bootstart.maven;
 
+import org.springframework.cglib.core.internal.Function;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -12,14 +13,23 @@ public class EujianRedisTemplate {
     }
 
     public String getString(String key){
-        Jedis resource = pool.getResource();
-        String o = resource.get(key);
-        return o;
+        String value = redisFun(jedis -> {
+            return jedis.get(key);
+        });
+        return value;
     }
 
     public void setString (String key,String object){
-        Jedis resource = pool.getResource();
-        resource.set(key,object);
+        redisFun(jedis -> {
+            jedis.set(key,object);
+            return null;
+        });
     }
 
+    public <T>T redisFun (Function<Jedis,T> function){
+        Jedis resource = pool.getResource();
+        T apply = function.apply(resource);
+        resource.close();
+        return apply;
+    }
 }
